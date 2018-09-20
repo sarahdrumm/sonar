4d64b07a1e105e5719257e72978af4c38b2ef840
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-20 09:58:44 -0500

    make columns responsive

--- sonar/src/Map.tsx
+++ sonar/src/Map.tsx
@@ -7,7 +7,7 @@ class Map extends React.Component {
                 <div className="clearfix" />
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-md-4 listing-block">
+                        <div className="col-sm-4 listing-block">
                             <div className="media">
                                 <div className="media-body pl-3">
                                     <div className="event">Lunch<small>12pm Jimmy John's</small></div>
@@ -45,7 +45,7 @@ class Map extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-8 map-box mx-0 px-0">
+                        <div className="col-sm-8 map-box mx-0 px-0">
                             <iframe width="100%" height="100%" scrolling="no" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed" />
                         </div>
                     </div>
