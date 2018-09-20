71c3f10ef42a32cd2ec032f3edf9ebf62e12d613
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-20 09:09:15 -0500

    add div

--- sonar/src/App.tsx
+++ sonar/src/App.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import './App.css';
-// import Buttons from './Button';
+import Buttons from './Button';
 import Map from './Map';
 
 class App extends React.Component {
   public render() {
     return (
-      // <Buttons/>
-      <Map/>
+      <div>
+        <Buttons/>
+        <Map/>
+      </div>
     );
   }
 }
