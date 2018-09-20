00f076dc95ea03e446e34d2d9f179513f6a4ad3c
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-20 09:29:12 -0500

    move map and import css

--- sonar/src/App.tsx
+++ sonar/src/App.tsx
@@ -3,6 +3,7 @@ import AddClickModal from './AddClickModal';
 import './App.css';
 import Buttons from './Button';
 import Map from './Map';
+import './Map.css'
 
 class App extends React.Component {
   public render() {
--- sonar/src/Map.tsx
+++ sonar/src/Map.tsx
@@ -34,11 +34,12 @@ class Map extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-md-7 map-box mx-0 px-0">
-                                <iframe width="100%" height="100%" scrolling="no" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"/>
-                            </div>
                         </div>
                     </div>
+                    <div className="col-md-7 map-box mx-0 px-0">
+                            <iframe width="100%" height="100%" scrolling="no" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"/>
+                    </div>
+
                 </section>
             </div>
         );
