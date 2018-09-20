d9f276efe6cb8c0d86f8aef3619e24a01b086474
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-20 09:32:04 -0500

    merge conflict fix

--- sonar/src/App.tsx
+++ sonar/src/App.tsx
@@ -4,11 +4,8 @@ import './App.css';
 import Buttons from './Button';
 import ClickModal from './ClickModal';
 import Map from './Map';
-<<<<<<< HEAD
 import './Map.css'
-=======
 import ProfileModal from './ProfileModal';
->>>>>>> 1faca7d30a338eb4a89d57e5bba7e2c1bec81e70
 
 class App extends React.Component {
   public render() {
