d6a8d29de1afbf67fabe3d108898a205c358d99e
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-20 14:35:19 -0500

    add search bar

--- sonar/src/Button.tsx
+++ sonar/src/Button.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 
 class Buttons extends React.Component {
-    public render() {
-      return (
-        <div className="col-md-4 topButtons">
-          <button type="button" className="fa fa-user fa-2x btn btn-info" data-toggle="modal" data-target="#profileModal"/>
-          <button type="button" className="fa fa-plus fa-2x btn btn-info" data-toggle="modal" data-target="#addClickModal"/>
-          <button type="button" className="fa fa-search fa-2x btn btn-info"/>
-        </div>
-      );
+  public render() {
+    return (
+      <div className="col-md-4 topButtons form-inline">
+        <button type="button" className="fa fa-user fa-2x btn btn-info" data-toggle="modal" data-target="#profileModal" />
+        <button type="button" className="fa fa-plus fa-2x btn btn-info" data-toggle="modal" data-target="#addClickModal" />
+        <button type="button" className="fa fa-search fa-2x btn btn-info" aria-hidden="true"/>
+        <form>
+          <input className="form-control form-control-sm search-size" type="text" placeholder="Search" aria-label="Search"/>
+        </form>      
+      </div>
+        );
+      }
     }
-  }
-  
-  export default Buttons;
+    
+export default Buttons;
