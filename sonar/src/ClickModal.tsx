fde859ebeba1d61d52908073244f36ae29d5725d
Rachel Hale <rachelhale@RHALE-MBP-SEA.local>
2018-09-21 09:52:36 -0500

    Changed the look of click modal

--- sonar/src/ClickModal.tsx
+++ sonar/src/ClickModal.tsx
@@ -8,23 +8,27 @@ class ClickModal extends React.Component {
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="clickModalLabel">Click Name</h5>
+                            <h3 className="modal-title" id="clickModalLabel">Lunch</h3>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div className="modal-body">
-                            <div className="form-group">
-                                <label htmlFor="Info">Info</label>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="date">Date</label>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="time">Time</label>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="location">Location</label>
+
+                            <div className="click">
+                                <button type="button" className="media-body pl-3 btn" data-toggle="modal" data-target="#clickModal">
+                                    <div className="row">
+                                        <div className="col col-sm-0">
+                                            <div className="event">Lunch</div>
+                                            <div className="event"><small>We're grabbing lunch at Starbucks, come join us!</small></div>
+                                            <div className="event"><small>11am Starbucks</small></div>
+                                            <div className="address">238 Winter Ave, Sacramento CA</div>
+                                        </div>
+                                        <div className="col col-sm-3 media-right">
+                                            <div className="date">9.10</div>
+                                        </div>
+                                    </div>
+                                </button>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-info">Join Click</button>
