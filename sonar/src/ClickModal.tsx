9acf057bd29e895be9c05ccd75079d2dca2b31c2
Drumm <sarah.drumm@parivedasolutions.com>
2018-09-21 10:26:38 -0500

    broken form validation

--- sonar/src/ClickModal.tsx
+++ sonar/src/ClickModal.tsx
@@ -3,28 +3,25 @@ import * as React from 'react';
 class ClickModal extends React.Component {
     public render() {
         return (
-
+            
             <div className="modal fade all-text" id="clickModal" tabIndex={-1} role="dialog" aria-labelledby="clickModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="clickModalLabel">Click Name</h5>
+                            <h5 className="modal-title" id="clickModalLabel">Lunch</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div className="modal-body">
                             <div className="form-group">
-                                <label htmlFor="Info">Info</label>
+                                <label htmlFor="date">Date: 9/10</label>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="date">Date</label>
+                                <label htmlFor="time">Time: 11am</label>
                             </div>
                             <div className="form-group">
-                                <label htmlFor="time">Time</label>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="location">Location</label>
+                                <label htmlFor="location">Location: 238 Winter Ave</label>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-info">Join Click</button>
--- sonar/src/Login.tsx
+++ sonar/src/Login.tsx
@@ -1,33 +1,60 @@
 import * as React from 'react';
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FormGroup } from 'react-bootstrap';
+import { ControlLabel } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 
 import './App.css';
 import './Login.css';
 import './Map.css';
 
 class Login extends React.Component {
+
+    constructor(props: any, context: any) {
+        super(props, context);
+
+        this.changeHandler = this.changeHandler.bind(this);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    changeHandler(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 0) return 'success';
+        return null;
+    }
+
     public render() {
         return (
             <div className="container centerL all-text">
+                <div className="login-text">
+                    Welcome to SONAR
+                </div>
                 <form>
-                    <div className="login-text">
-                            Welcome to SONAR
-                    </div>
-
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                    </div>
-                    <Link to="/home">
-                        <button type="submit" className="btn btn-info link-text">
-                            Login
-                        </button>
-                    </Link>
+                    <FormGroup
+                        validationState={this.getValidationState()}
+                    >
+                            <ControlLabel>Email address</ControlLabel>
+                            <FormControl
+                                type="text"
+                                value={this.state.value}
+                                placeholder="Enter email address"
+                                onChange={this.changeHandler}
+                            />
+                        <FormControl.Feedback />
+                    </FormGroup>
                 </form>
+                <Link to="/home">
+                    <button type="submit" className="btn btn-info link-text">
+                        Login
+                        </button>
+                </Link>
             </div>
         );
     }
